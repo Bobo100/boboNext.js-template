21b2999c035bf5746e58ab629f85e7db2ff3dae6
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-31 10:01:17 +0800

    v 0.2.0 新增樣式

---
+++ template/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import RouterLink from "./RoutesLink"
+
+const NavBar = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            const visible = prevScrollPos > currentScrollPos;
+
+            setPrevScrollPos(currentScrollPos);
+            setVisible(visible);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [prevScrollPos, visible]);
+
+    return (
+        <nav className={`navbar ${visible ? 'navbar--visible' : 'navbar--hidden'}`}>
+            <RouterLink />
+        </nav>
+    )
+}
+
+export default NavBar
--- template/components/layout.tsx
+++ template/components/layout.tsx
@@ -1,6 +1,15 @@
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+
 interface LayoutProps {
     children: React.ReactNode;
 }
 export default function Layout({ children }: LayoutProps) {
-    return <div className="App">{children}</div>;
+    return (<div className="App">
+        <NavBar />
+        <div className="container">
+            {children}
+        </div>
+        <Footer />
+    </div>);
 }
