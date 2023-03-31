0ee497449d4ed14429cbe3652fd772abfbd5ad3b
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-31 17:58:09 +0800

    新增樣式

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
     // "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...require("tailwindcss/colors"), // 使用展開運算子得到所有預設的顏色屬性
+      title: "#61dafb", // 自定義 title 顏色
+    },
     extend: {},
   },
   plugins: [],
