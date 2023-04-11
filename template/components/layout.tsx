22276999e3c98a564f40e1998f58d88cb557a963
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-04-11 11:22:53 +0800

    更新版本 樣式

--- template/components/layout.tsx
+++ template/components/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
     return (<div className="App">
         <NavBar />
-        <div className="container">
+        <div className="pt-20 px-3">
             {children}
         </div>
         <Footer />
